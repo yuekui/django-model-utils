f7f6861bf7d8a634a96251add5ed56d594fa8129
yuekui <liyuekui@gmail.com>
2023-06-15 14:55:16 -0700

    Adjust order

--- model_utils/tracker.py
+++ model_utils/tracker.py
@@ -344,8 +344,8 @@ class FieldTracker:
         models.signals.post_init.connect(self.initialize_tracker, sender=sender)
         
         def init_subclass(cls, finalize_class=self.finalize_class, parent_cls=sender, **kwargs):
-            super(parent_cls, cls).__init_subclass__(**kwargs)
             models.signals.class_prepared.connect(finalize_class, sender=cls)
+            super(parent_cls, cls).__init_subclass__(**kwargs)
             
         sender.__init_subclass__ = classmethod(init_subclass)
         self.model_class = sender
